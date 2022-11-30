6dc409e8cab37b2afb74992924ca489948179122
Adam McGregor <mcgregorada@deakin.edu.au>
2022-11-30 23:29:06 +1100

    Add a linkage tree data structure

---
+++ AGM.OptimisationTools/ModelBasedLearning/LinkageTree.cs
@@ -0,0 +1,115 @@
+﻿using AGM.OptimisationTools.CombinatorialReserve;
+
+namespace AGM.OptimisationTools.ModelBasedLearning;
+
+public delegate double Distance<T>(ref T x, ref T y) where T : struct, IReserved;
+
+/// <summary>
+/// A linkage tree or hierarchical cluster
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public sealed class LinkageTree<T> where T : struct, IReserved
+{
+    private readonly ICombinatorialMemoryReserve<T> _reserve;
+    public Stack<int> Stack { get; }
+
+    public LinkageTree(ICombinatorialMemoryReserve<T> reserve, Distance<T> distance)
+    {
+        _reserve = reserve;
+        Span<int> clusters = stackalloc int[2 * reserve.N - 1];
+        byte ClustersCount = reserve.N;
+        Stack = new Stack<int>(clusters.Length);
+        for (byte i = 0; i < ClustersCount; i++)
+        {
+            Stack.Push(i);
+            clusters[i] = i;
+            ref var reserved = ref reserve[i];
+            if (!reserved.Initialised)
+            {
+                reserved.Ids = new byte[] { i };
+                reserved.Initialise();
+            }
+        }
+
+
+        Span<double> proximity = stackalloc double[reserve.N * ( reserve.N + 1 ) / 2];
+        Dictionary<int, byte> dict = new(reserve.N + 1);
+
+        double min = double.MaxValue;
+        int x = 0, y = 0; // min value keys
+                          // initialise proximity matrix
+        for (byte i = 0; i < reserve.N; i++)
+        {
+            dict.Add(i, i);
+            for (byte j = (byte)( i + 1 ); j < reserve.N; j++)
+            {
+                int index = GetIndex(i, j, reserve.N);
+                proximity[index] = distance(ref reserve[i], ref reserve[j]);
+                if (proximity[index] < min)
+                {
+                    min = proximity[index];
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        // build clusters while all clusters are not merged or are not able to be mergered
+        Span<byte> cluster = stackalloc byte[reserve.N];
+        while (dict.Count > 1 && min != double.MaxValue)
+        {
+            if (distance(ref reserve[x], ref reserve[y]) == double.MaxValue)
+            {
+                proximity[GetIndex(dict[x], dict[y], reserve.N)] = double.MaxValue;
+            }
+            else
+            {
+                // build new cluster
+                var cx = reserve[clusters[x]].Ids.Span;
+                var cy = reserve[clusters[y]].Ids.Span;
+                Span<byte> newCluster = cluster[..( cx.Length + cy.Length )];
+                for (int i = 0, j = 0, k = 0; i < cx.Length + cy.Length; i++)
+                {
+                    newCluster[i] = j < cx.Length && ( k >= cy.Length || cx[j] < cy[k] ) ? cx[j++] : cy[k++];
+                }
+                int id = reserve.GetId(newCluster);
+                Stack.Push(id);
+                clusters[ClustersCount++] = id;
+
+                // update proximity matrix
+                var c1 = clusters[dict[x]];
+                foreach (var i in dict.Keys.Where(p => dict[p] > dict[x]))
+                {
+                    var c2 = clusters[dict[i]];
+                    int index = GetIndex(dict[x], dict[i], reserve.N);
+                    proximity[index] = distance(ref reserve[c1], ref reserve[c2]);
+                }
+                // add cluster c_ij to matrix
+                dict.Add(ClustersCount - 1, dict[x]);
+                // remove clusters c_i and c_j to matrix
+                dict.Remove(x);
+                dict.Remove(y);
+                // if there is more than one cluster, no more to merge
+                if (dict.Count <= 1)
+                    break;
+            }
+
+            min = double.MaxValue;
+            foreach (var i in dict.Keys)
+            {
+                foreach (var j in dict.Keys.Where(p => dict[p] > dict[i]))
+                {
+                    int index = GetIndex(dict[i], dict[j], reserve.N);
+                    if (proximity[index] < min)
+                    {
+                        min = proximity[index];
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private static int GetIndex(byte i, byte j, byte length) => i * length + j - ( i + 1 ) * ( i + 2 ) / 2;
+}
---
+++ Tests/ModelBasedLearning/LinkageTreeTests.cs
@@ -0,0 +1,85 @@
+﻿using AGM.OptimisationTools.CombinatorialReserve;
+using AGM.OptimisationTools.ModelBasedLearning;
+
+namespace Tests.ModelBasedLearning;
+
+public class LinkageTreeTests
+{
+    [Fact]
+    public void IdsCorrectlySorted()
+    {
+        byte n = 5;
+        CombinatorialMemoryReserve<TestStruct> reserve = new(n);
+        LinkageTree<TestStruct> linkageTree = new(reserve, TestStruct.Distance);
+
+        var stack = linkageTree.Stack;
+        foreach (var item in stack)
+        {
+            var ids = reserve[item].Ids.Span;
+            for (int i = 0; i < ids.Length - 1; i++)
+            {
+                Assert.True(ids[i] < ids[i + 1]);
+            }
+        }
+    }
+
+
+    [Fact]
+    // https://medium.com/@rohanjoseph_91119/learn-with-an-example-hierarchical-clustering-873b5b50890c
+    public void CorrectDendogram()
+    {
+        const byte n = 5;
+        CombinatorialMemoryReserve<TestStruct> reserve = new(n);
+
+        int[] ReserveIds = new int[2 * n - 1];
+        byte[][] combos = new byte[2 * n - 1][]
+        {
+            new byte[]{ 0, 1, 2, 3, 4 },
+            new byte[]{ 2, 3, 4 },
+            new byte[]{ 3, 4 },
+            new byte[]{ 0, 1 },
+            new byte[]{ 4 },
+            new byte[]{ 3 },
+            new byte[]{ 2 },
+            new byte[]{ 1 },
+            new byte[]{ 0 }
+        };
+
+        LinkageTree<TestStruct> linkageTree = new(reserve, TestStruct.Distance);
+
+        for (int i = 0; i < ReserveIds.Length; i++)
+        {
+            ReserveIds[i] = reserve.GetId(combos[i]);
+        }
+
+        var stack = linkageTree.Stack;
+        for (int i = 0; i < ReserveIds.Length; i++)
+        {
+            Assert.Equal(ReserveIds[i], stack.Pop());
+        }
+    }
+}
+
+file static class Data
+{
+    public static int[] Points = new[] { 7, 10, 20, 28, 35 };
+}
+
+struct TestStruct : IReserved
+{
+    public int Point { get; set; }
+    public Memory<byte> Ids { get; set; }
+
+    public void Initialise()
+    {
+        int sum = 0;
+        Span<byte> ids = Ids.Span;
+        for (int i = 0; i < ids.Length; i++)
+        {
+            sum += Data.Points[ids[i]];
+        }
+        Point = sum / ids.Length;
+    }
+
+    public static double Distance(ref TestStruct x, ref TestStruct y) => y.Point - x.Point;
+}
